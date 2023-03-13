db268ebd41572a80a08f8858173e68dd425bdc6a
rosaidabhz <rosaidabhz@gmail.com>
2023-03-13 09:37:20 -0500

    changes templates and views

--- cdj/urls.py
+++ cdj/urls.py
@@ -22,5 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('search_consejos/', views.search_consejos, name='search_consejos'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
